20ba72dd1462f6343ae3e2266d9ab02ecadb15ae
Cato Teig <kno-catei@fiskeridir.no>
2022-12-28 00:04:43 +0100

    day_04_part_2

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -3,15 +3,18 @@ def main():
     with open('input.txt') as f:
         assigment_pairs = [[[int(elf) for elf in pair.split('-')] for pair in line.split(',')] for line in f.read().splitlines()]
 
-    total = 0
+    total, total_2 = 0, 0
     for elf_1, elf_2 in assigment_pairs:
         id_1 = [_ for _ in range(elf_1[0], elf_1[1]+1)]
         id_2 = [_ for _ in range(elf_2[0], elf_2[1]+1)]
 
         if all(item in id_1 for item in id_2) or all(item in id_2 for item in id_1):
             total += 1
+        if any(item in id_1 for item in id_2) or any(item in id_2 for item in id_1):
+            total_2 += 1
 
     print(total)
+    print(total_2)
 
 
 if __name__ == '__main__':
