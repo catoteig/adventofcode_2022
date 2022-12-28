8fa9b7efa562ed7fbcf4ae5f90bae178b9828271
Cato Teig <kno-catei@fiskeridir.no>
2022-12-28 23:10:17 +0100

    day_06_part_2

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -3,10 +3,18 @@ def main():
     with open('input.txt') as f:
         datastream = [_ for _ in f.read().splitlines()]
 
+    # Part 1
     for stream in datastream:
         for i in range(len(stream)):
             if len(set(stream[i:i+4])) == 4:
                 print(i+4)
+                break
+
+    # Part 2
+    for stream in datastream:
+        for i in range(len(stream)):
+            if len(set(stream[i:i+14])) == 14:
+                print(i+14)
                 break
 
 
