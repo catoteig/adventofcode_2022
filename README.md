38909dcf9c157744b6e365a368a1553c5bdbb621
Cato Teig <kno-catei@fiskeridir.no>
2023-01-01 21:53:34 +0100

    day_09_part_2

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 https://adventofcode.com/
 
-| Day | Stars        | Day | Stars        | 
-|----:|:-------------|----:|--------------|
-|   1 | :star::star: |  13 |              |
-|   2 | :star::star: |  14 |              |
-|   3 | :star::star: |  15 |              |
-|   4 | :star::star: |  16 |              |
-|   5 | :star::star: |  17 |              |
-|   6 | :star::star: |  18 |              |
-|   7 | :star::star: |  19 |              |
-|   8 | :star::star: |  20 |              |
-|   9 | :star:       |  21 |              |
-|  10 |              |  22 |              |
-|  11 |              |  23 |              |
-|  12 |              |  24 |              |
+| Day |              | Day |     | 
+|----:|:-------------|----:|-----|
+|   1 | :star::star: |  13 |     |
+|   2 | :star::star: |  14 |     |
+|   3 | :star::star: |  15 |     |
+|   4 | :star::star: |  16 |     |
+|   5 | :star::star: |  17 |     |
+|   6 | :star::star: |  18 |     |
+|   7 | :star::star: |  19 |     |
+|   8 | :star::star: |  20 |     |
+|   9 | :star::star: |  21 |     |
+|  10 |              |  22 |     |
+|  11 |              |  23 |     |
+|  12 |              |  24 |     |
--- day_09/day_09.py
+++ day_09/day_09.py
@@ -3,8 +3,10 @@ def main():
     with open('input.txt') as f:
         motions = [tuple(int(i) if i.isdigit() else i for i in _.split(' ')) for _ in f.read().splitlines()]
 
-    tail_log = set()
-    head_x, head_y, tail_x, tail_y = 0, 0, 0, 0
+    tail_length = 9  # <--- Part 1: Length = 1 ; Part 2: Length = 9
+    tail = [(0, 0)] * tail_length
+    tail_end_log = set()
+    head_x, head_y = 0, 0
 
     for direction, steps in motions:
         for step in range(steps):
@@ -14,24 +16,35 @@ def main():
             if direction == 'U': head_y += 1
             if direction == 'D': head_y -= 1
 
-            # Tail:
-            if (tail_x, tail_y) == (head_x, head_y):  # Head and tail on same spot
-                pass
-            elif abs(tail_x - head_x) <= 1 and abs(tail_y - head_y) <= 1:  # Head and tail touching
-                pass
-            elif tail_x == head_x and abs(tail_y - head_y) > 1:  # Vertical move
-                tail_y += 1 if direction == 'U' else -1
-            elif tail_y == head_y and abs(tail_x - head_x) > 1:  # Horizontal move
-                tail_x += 1 if direction == 'R' else -1
-            elif abs(head_x - tail_x) >= 2 and abs(head_y - tail_y) >= 1:  # Diagonal horizontal move
-                tail_x += 1 if direction == 'R' else -1
-                tail_y = head_y
-            elif abs(head_x - tail_x) >= 1 and abs(head_y - tail_y) >= 2:  # Diagonal vertical move
-                tail_y += 1 if direction == 'U' else -1
-                tail_x = head_x
-            tail_log.add((tail_x, tail_y))
+            temp_head_x, temp_head_y = head_x, head_y
 
-    print(len(tail_log))
+            for idx, (tail_x, tail_y) in enumerate(tail):
+                if (tail_x, tail_y) == (temp_head_x, temp_head_y):  # Head and tail on same spot
+                    pass
+                elif abs(tail_x - temp_head_x) <= 1 and abs(tail_y - temp_head_y) <= 1:  # Head and tail touching
+                    pass
+                elif tail_x == temp_head_x and abs(tail_y - temp_head_y) > 1:  # Vertical move
+                    tail_y += 1 if (temp_head_y > tail_y) else -1
+                elif tail_y == temp_head_y and abs(tail_x - temp_head_x) > 1:  # Horizontal move
+                    tail_x += 1 if (temp_head_x > tail_x) else -1
+                elif abs(temp_head_x - tail_x) >= 1 and abs(temp_head_y - tail_y) >= 1:  # Diagonal move
+                    if temp_head_y > tail_y:
+                        tail_y += 1
+                    elif temp_head_y < tail_y:
+                        tail_y -= 1
+
+                    if temp_head_x > tail_x:
+                        tail_x += 1
+                    elif temp_head_x < tail_x:
+                        tail_x -= 1
+
+                temp_head_x, temp_head_y = tail_x, tail_y
+                tail[idx] = (tail_x, tail_y)
+
+                if idx == len(tail)-1:
+                    tail_end_log.add((tail_x, tail_y))
+
+    print(len(tail_end_log))
 
 
 if __name__ == '__main__':
