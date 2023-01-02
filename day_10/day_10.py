651b1b80cd6fefe1763dce9747cdb436ee7f6e13
Cato Teig <kno-catei@fiskeridir.no>
2023-01-02 19:57:38 +0100

    day_10_part_2

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -9,8 +9,19 @@ def main():
     cycle_list = cycle(instructions)
     check_at_cycle, check_result = [20, 60, 100, 140, 180, 220], []
     x, addx, skip = 1.0, 0.0, False
+    crt_dimensions, crt_plot = (40, 6), []
+    lit, dark = '#', '.'
 
-    for cycle_num in range(1, check_at_cycle[-1]+1):
+    for cycle_num, pixel_drawn in zip(
+            range(1, crt_dimensions[0]*crt_dimensions[1]+1),
+            cycle(list(range(crt_dimensions[0])))):
+
+        sprite_pos = [x - 1, x, x + 1]
+        if pixel_drawn in sprite_pos:
+            crt_plot.append(lit)
+        else:
+            crt_plot.append(dark)
+
         if cycle_num in check_at_cycle:
             check_result.append(cycle_num * x)
 
@@ -22,7 +33,11 @@ def main():
             if instruction[0] == 'addx':
                 addx, skip = instruction[1], True
 
+    # Part 1
     print(sum(check_result))
+
+    # Part 2
+    for _ in range(0, len(crt_plot), 40): print(''.join(crt_plot[_:_+40]))
 
 
 if __name__ == '__main__':
